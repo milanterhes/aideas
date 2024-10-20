bcce07eab39a95aba57400b94a46fa63955447c0
Milan Terhes <milan.terhes@eloop.app>
2024-10-20 14:57:25 +0200

    cleanup migrations

--- aideas/drizzle.config.ts
+++ aideas/drizzle.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local",
+});
+
+const databaseUrl = process.env.DATABASE_URL;
+const databaseAuthToken = process.env.DATABASE_AUTH_TOKEN;
+
+if (!databaseUrl || !databaseAuthToken) {
+  throw new Error("DATABASE_URL and DATABASE_AUTH_TOKEN must be set");
+}
 
 export default defineConfig({
-    dialect: "sqlite",
-    dbCredentials: {
-        url: "file:local.db",
-    },
-    schema: "src/server/schema.ts",
-    strict: true,
-    verbose: true,
-    out: "./migrations",
+  dialect: "turso",
+  dbCredentials: {
+    url: databaseUrl,
+    authToken: databaseAuthToken,
+  },
+  schema: "src/server/schema.ts",
+  strict: true,
+  verbose: true,
+  out: "./migrations",
 });
